3df39080a6a9346463c736c4516adb5d1dbb88c6
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-05 21:42:40 +0200

    Added basic table definition for equipment

--- mainPages/models.py
+++ mainPages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+# Equipment datatable
+class EquipmentStock(models.Model):
+    manufacturer = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=13)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return str(self.manufacturer) + " " + str(self.model_name)
