be8e6d09fcbcc56bad4eb598e1619a8314489bbb
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-06 09:10:19 +0200

    Registered model with admin class and created basic tests

--- mainPages/tests.py
+++ mainPages/tests.py
@@ -1,57 +1,25 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
 from django.urls import reverse
+from .models import EquipmentStock
 
 
-class HomepageTests(SimpleTestCase):
+class EquipmentStockTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.equipmentstock = EquipmentStock.objects.create(
+            manufacturer="Shure", model_name="SM-58"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.equipmentstock.manufacturer, "Shure")
+        self.assertEqual(self.equipmentstock.model_name, "SM-58")
+
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):
+    def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "home.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Homepage</h1>")
-
-
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("about"))
-        self.assertTemplateUsed(response, "about.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>About Us Page</h1>")
-
-
-class ContactpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/contact")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("contact"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("contact"))
-        self.assertTemplateUsed(response, "contact.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("contact"))
-        self.assertContains(response, "<h1>Contact Us Page</h1>")
