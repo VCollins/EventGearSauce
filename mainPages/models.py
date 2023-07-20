6842af903f95b462eee12abe8ee6e027433cce5d
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-20 16:03:24 +0200

    Added equipment stock list and equipment stock detail pages for equipment review

--- mainPages/models.py
+++ mainPages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -6,8 +7,12 @@ from django.db import models
 class EquipmentStock(models.Model):
     manufacturer = models.CharField(max_length=50)
     model_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
     serial_number = models.CharField(max_length=13)
-    amount = models.IntegerField()
+    amount_available = models.IntegerField()
 
     def __str__(self):
         return str(self.manufacturer) + " " + str(self.model_name)
+
+    def get_absolute_url(self):
+        return reverse("equipment_stock_detail", kwargs={"pk": self.pk})
--- mainPages/views.py
+++ mainPages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import EquipmentStock
 
 
 class HomePageView(TemplateView):
+    model = EquipmentStock
     template_name = "home.html"
+
+
+class EquipmentStockListView(ListView):
+    model = EquipmentStock
+    template_name = "equipment_stock_list.html"
+
+
+class EquipmentStockDetailView(DetailView):
+    model = EquipmentStock
+    template_name = "equipment_stock_detail.html"
 
 
 class AboutPageView(TemplateView):
