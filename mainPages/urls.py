45edfdf90ea61ff4737498689f9e4ab4c2372c43
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-20 17:09:08 +0200

    Updated urls to match patterns for locating equipment information

--- mainPages/urls.py
+++ mainPages/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import (
+    HomePageView,
+    AboutPageView,
+    ContactPageView,
+    EquipmentStockListView,
+    EquipmentStockDetailView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path(
+        "equipment_stock", EquipmentStockListView.as_view(), name="equipment_stock_list"
+    ),
+    path(
+        "equipment_stock/<int:pk>",
+        EquipmentStockDetailView.as_view(),
+        name="equipment_stock_detail",
+    ),
     path("about", AboutPageView.as_view(), name="about"),
     path("contact", ContactPageView.as_view(), name="contact"),
 ]
