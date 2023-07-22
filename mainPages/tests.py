177c66dba23f0d30bb360045f451af3de6954158
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-22 20:51:44 +0200

    Modified colour scheme and layout

--- mainPages/tests.py
+++ mainPages/tests.py
@@ -10,15 +10,19 @@ class EquipmentStockTests(TestCase):
         cls.equipmentstock = EquipmentStock.objects.create(
             manufacturer="Shure",
             model_name="SM-58",
+            description="This is a useful little microphone",
             serial_number="1234123412340",
-            amount=10,
+            amount_available=10,
         )
 
     def test_model_content(self):
         self.assertEqual(self.equipmentstock.manufacturer, "Shure")
         self.assertEqual(self.equipmentstock.model_name, "SM-58")
         self.assertEqual(self.equipmentstock.serial_number, "1234123412340")
-        self.assertEqual(self.equipmentstock.amount, 10)
+        self.assertEqual(self.equipmentstock.amount_available, 10)
+        self.assertEqual(
+            self.equipmentstock.description, "This is a useful little microphone"
+        )
 
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
