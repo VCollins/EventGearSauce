6d885bc0575cb1e321b1a9a8c36d64e5985d40e4
Vincent Collins <vincent.collins.83523@gmail.com>
2023-07-22 20:24:25 +0200

    Added paths to urls file for CRUD operations

--- mainPages/urls.py
+++ mainPages/urls.py
@@ -5,6 +5,9 @@ from .views import (
     ContactPageView,
     EquipmentStockListView,
     EquipmentStockDetailView,
+    EquipmentStockCreateView,
+    EquipmentStockUpdateView,
+    EquipmentStockDeleteView,
 )
 
 urlpatterns = [
@@ -16,6 +19,21 @@ urlpatterns = [
         "equipment_stock/<int:pk>",
         EquipmentStockDetailView.as_view(),
         name="equipment_stock_detail",
+    ),
+    path(
+        "equipment_stock/new/",
+        EquipmentStockCreateView.as_view(),
+        name="equipment_stock_new",
+    ),
+    path(
+        "equipment_stock/<int:pk>/edit/",
+        EquipmentStockUpdateView.as_view(),
+        name="equipment_stock_edit",
+    ),
+    path(
+        "equipment_stock/<int:pk>/delete/",
+        EquipmentStockDeleteView.as_view(),
+        name="equipment_stock_delete",
     ),
     path("about", AboutPageView.as_view(), name="about"),
     path("contact", ContactPageView.as_view(), name="contact"),
