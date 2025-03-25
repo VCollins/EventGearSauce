76dba43971c9da738334ba897ee7bbda619b6a1b
VCollins <vincent.collins.83523@gmail.com>
2025-03-25 10:58:38 +0200

    Updated models and serializers, now using many to many relationship

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,7 +9,8 @@ class EquipmentStockItem(models.Model):
     model_name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default=None)
     serial_number = models.CharField(max_length=13)
-    amount_available = models.IntegerField(default=0)
+    amount_available = models.PositiveIntegerField(default=0)
+    rental_cost = models.PositiveIntegerField(default=0)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="equipmentstockitem")
 
     def __str__(self):
@@ -17,3 +18,17 @@ class EquipmentStockItem(models.Model):
 
     def get_absolute_url(self):
         return reverse("equipment_stock_detail", kwargs={"pk": self.pk})
+
+class Quotation(models.Model):
+    quote_number = models.PositiveIntegerField(default=1, primary_key=True)
+    date_created = models.DateField(auto_now_add=True)
+    client = models.CharField(max_length=100)
+    equipment = models.ManyToManyField(EquipmentStockItem)
+    total_quote_amount = models.PositiveIntegerField(default=0)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quotation")
+
+    def __str__(self):
+        return str("Quote number: "+ str(self.quote_number))
+
+    def get_absolute_url(self):
+        return reverse("quote_detail", kwargs={"pk": self.quote_number})
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import EquipmentStockItem
+from .models import EquipmentStockItem, Quotation
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,5 +16,11 @@ class UserSerializer(serializers.ModelSerializer):
 class EquipmentStockItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentStockItem
-        fields = ["id", "manufacturer", "model_name", "description", "serial_number", "amount_available", "creator"]
+        fields = ["id", "manufacturer", "model_name", "description", "serial_number", "amount_available", "rental_cost", "creator"]
+        extra_kwargs = {"creator": {"read_only": True}}
+
+class QuotationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quotation
+        fields = ["quote_number", "date_created", "client", "equipment", "total_quote_amount", "creator"]
         extra_kwargs = {"creator": {"read_only": True}}
