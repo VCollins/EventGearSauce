c1a7773183776885c2f4aa6bea39fe155a57ff3c
VCollins <vincent.collins.83523@gmail.com>
2025-04-02 22:56:27 +0200

    Rebuilt frontend project after loading NPM packages incorrectly

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+    return <div>
+        <h1>The premier sound equipment rental specialists</h1>
+        <br />
+        <p>
+            We are a one-stop source for sound and audiovisual equipment rental for live events in Johannesburg, South Africa. Whether your event is a corporate presentation of a new product, 
+            or a live show bringing in significant talent to entertain your guests and fans, EventGearSauce has what you need to make your event a success!
+        </p>
+        <br />
+        <p>
+            We also engage with industry specialists on your behalf, should you require that special touch to make your event extraordinary!
+        </p>
+        <br />
+    </div>
+}
+
+export default Home
